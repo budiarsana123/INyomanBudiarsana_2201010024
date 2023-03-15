7fc7ae8fa2c0f84d34311ba2cb00a8463d83cdcc
cloud <cloud@34.124.223.51>
2023-03-15 09:51:04 +0800

    first commit

---
+++ sesi1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Sesi 1</title>
+
+
+</head>
+<body>
+    <h3>Latihan web programing</h3>
+
+    <script>
+        document.write("Latihan web programing - dg Javascript");
+    </script>
+
+    <br>
+    <?php
+    echo "Latihan web programing - dg PHP";
+    ?>
+    
+</body>
+</html> 
